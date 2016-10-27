b02811e8819bea572fe7d67ebd0007460c657186
girardi <girardi.fmce@imbel.gov.br>
2016-10-27 15:05:54 -0200

    Ajustes para o ENEM 2016.

--- src/com/romullogirardi/checker/model/AnswersCardReader.java
+++ src/com/romullogirardi/checker/model/AnswersCardReader.java
@@ -12,13 +12,21 @@ import com.romullogirardi.checker.model.Enums.QuestionOptionLetters;
 public class AnswersCardReader {
 
 	//CONSTANTS
-	private int FIRST_ROW_Y_POSITION = 88;
-	private float ROW_DISTANCE = 31.5f;
-	private int FIRST_COLUMN_X_POSITION = 65;
-	private int COLUMN_DISTANCE = 220;
-	private int COLUMN_MIDDLE_GAP = 14;
-	private int ANSWERS_X_DISTANCE = 34;
-	private int MARKING_DIMENSION = 4;
+	private int FIRST_ROW_Y_POSITION_1 = 1612;
+	private float ROW_DISTANCE_1 = 31.5f;
+	private int FIRST_COLUMN_X_POSITION_1 = 215;
+	private int COLUMN_DISTANCE_1 = 220;
+	private int ANSWERS_X_DISTANCE_1 = 33;
+	private int COLUMN_MIDDLE_GAP_1 = 14;
+	
+	private int FIRST_ROW_Y_POSITION_2 = 1616;
+	private float ROW_DISTANCE_2 = 31.8f;
+	private int FIRST_COLUMN_X_POSITION_2 = 216;
+	private int COLUMN_DISTANCE_2 = 223;
+	private int ANSWERS_X_DISTANCE_2 = 34;
+	private int COLUMN_MIDDLE_GAP_2 = 12;
+	
+	private int MARKING_DIMENSION = 5;
 	private int COLOR_REFERENCE = 180;
 	
 	//ATTRIBUTE
@@ -37,7 +45,7 @@ public class AnswersCardReader {
 			if(!questionOptionLetter.equals(QuestionOptionLetters.VAZIA) && isOptionSelected(questionNumber, questionOptionLetter))
 				selectedOption = (selectedOption.equals(QuestionOptionLetters.VAZIA)) ? questionOptionLetter : QuestionOptionLetters.VAZIA;
 		}
-		System.out.println(questionNumber + " - " + selectedOption);
+//		System.out.println(questionNumber + " - " + selectedOption);
 		return selectedOption;
 	}
 	
@@ -65,57 +73,65 @@ public class AnswersCardReader {
 				break;
 		}
 
-		System.out.println(questionNumber + "-" + questionOptionLetter + "(" + (getFirstOptionPosition(questionNumber).getX() + (numberOfXJumps * ANSWERS_X_DISTANCE)) + 
-				", " + getFirstOptionPosition(questionNumber).getY() + ") - " +
-				(((isSelected(getFirstOptionPosition(questionNumber).getX() + (numberOfXJumps * ANSWERS_X_DISTANCE), getFirstOptionPosition(questionNumber).getY())) ? 
-				"MARCADO" : "DESMARCADO")));
-		return isSelected((getFirstOptionPosition(questionNumber).getX() + (numberOfXJumps * ANSWERS_X_DISTANCE)), getFirstOptionPosition(questionNumber).getY());
+		int answersXDistance = (questionNumber > 90) ? ANSWERS_X_DISTANCE_2 : ANSWERS_X_DISTANCE_1;
+
+//		System.out.println(questionNumber + "-" + questionOptionLetter + "(" + (getFirstOptionPosition(questionNumber).getX() + (numberOfXJumps * ANSWERS_X_DISTANCE_1)) + 
+//				", " + getFirstOptionPosition(questionNumber).getY() + ") - " +
+//				(((isSelected(getFirstOptionPosition(questionNumber).getX() + (numberOfXJumps * answersXDistance), getFirstOptionPosition(questionNumber).getY())) ? 
+//				"MARCADO" : "DESMARCADO")));
+
+		return isSelected((getFirstOptionPosition(questionNumber).getX() + (numberOfXJumps * answersXDistance)), getFirstOptionPosition(questionNumber).getY());
 	}
 	
 	private AnswersCardPosition getFirstOptionPosition(int questionNumber) {
 
 		int numberOfXJumps = 0;
 		int numberOfYJumps = 0;
-		int columnMiddleGap = 0;
+		int firstColumnXPosition = (questionNumber > 90) ? FIRST_COLUMN_X_POSITION_2 : FIRST_COLUMN_X_POSITION_1;
+		int columnDistance = (questionNumber > 90) ? COLUMN_DISTANCE_2 : COLUMN_DISTANCE_1;
+		int firstRowYPosition = (questionNumber > 90) ? FIRST_ROW_Y_POSITION_2 : FIRST_ROW_Y_POSITION_1;
+		float rowDistance = (questionNumber > 90) ? ROW_DISTANCE_2 : ROW_DISTANCE_1;
+		int columnMiddleGap = (questionNumber > 90) ? COLUMN_MIDDLE_GAP_2 : COLUMN_MIDDLE_GAP_1;
 		if((1 <= questionNumber && questionNumber <= 15) || (91 <= questionNumber && questionNumber <= 105)) {
 			if(91 <= questionNumber && questionNumber <= 105)
 				questionNumber -= 90;
 			numberOfYJumps = questionNumber - 1;
+			columnMiddleGap = 0;
 		}
 		else if((16 <= questionNumber && questionNumber <= 30) || (106 <= questionNumber && questionNumber <= 120)) {
 			if(106 <= questionNumber && questionNumber <= 120)
 				questionNumber -= 90;
 			numberOfXJumps = 1;
 			numberOfYJumps = questionNumber - 16;
+			columnMiddleGap = 0;
 		}
 		else if((31 <= questionNumber && questionNumber <= 45) || (121 <= questionNumber && questionNumber <= 135)) {
 			if(121 <= questionNumber && questionNumber <= 135)
 				questionNumber -= 90;
 			numberOfXJumps = 2;
 			numberOfYJumps = questionNumber - 31;
+			columnMiddleGap = 0;
 		}
 		else if((46 <= questionNumber && questionNumber <= 60) || (136 <= questionNumber && questionNumber <= 150)) {
 			if(136 <= questionNumber && questionNumber <= 150)
 				questionNumber -= 90;
 			numberOfXJumps = 3;
 			numberOfYJumps = questionNumber - 46;
-			columnMiddleGap = COLUMN_MIDDLE_GAP;
 		}
 		else if((61 <= questionNumber && questionNumber <= 75) || (151 <= questionNumber && questionNumber <= 165)) {
 			if(151 <= questionNumber && questionNumber <= 165)
 				questionNumber -= 90;
 			numberOfXJumps = 4;
 			numberOfYJumps = questionNumber - 61;
-			columnMiddleGap = COLUMN_MIDDLE_GAP;
 		}
 		else if((76 <= questionNumber && questionNumber <= 90) || (166 <= questionNumber && questionNumber <= 180)) {
 			if(166 <= questionNumber && questionNumber <= 180)
 				questionNumber -= 90;
 			numberOfXJumps = 5;
 			numberOfYJumps = questionNumber - 76;
-			columnMiddleGap = COLUMN_MIDDLE_GAP;
 		}
-		return new AnswersCardPosition(FIRST_COLUMN_X_POSITION + (numberOfXJumps * COLUMN_DISTANCE) + columnMiddleGap, Math.round(FIRST_ROW_Y_POSITION + (numberOfYJumps * ROW_DISTANCE)));
+		
+		return new AnswersCardPosition(firstColumnXPosition + (numberOfXJumps * columnDistance) + columnMiddleGap, Math.round(firstRowYPosition + (numberOfYJumps * rowDistance)));
 	}
 	
 	private boolean isSelected(int x, int y) {
